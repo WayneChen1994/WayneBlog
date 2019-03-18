45ba408e8e44cc4990bdcab9a99ff8fde79eb309
WayneChen1994 <waynechen1994@163.com>
2019-03-18 14:41:00 +0800

    根据需求定制Admin并抽取Admin基类

--- WayneBlog/comment/admin.py
+++ WayneBlog/comment/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Comment
+from WayneBlog.base_admin import BaseOwnerAdmin
+from WayneBlog.custom_site import custom_site
 
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
+@admin.register(Comment, site=custom_site)
+class CommentAdmin(BaseOwnerAdmin):
     list_display = ('target', 'nickname', 'content', 'website', 'created_time')
