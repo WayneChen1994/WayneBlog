fd3965ab67cc35023603fa006fb8076912874236
WayneChen1994 <waynechen1994@163.com>
2019-03-22 21:27:59 +0800

    配置RSS和sitemap

--- WayneBlog/blog/models.py
+++ WayneBlog/blog/models.py
@@ -1,6 +1,7 @@
 import mistune
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.functional import cached_property
 
 
 class Category(models.Model):
@@ -125,3 +126,7 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         self.content_html = mistune.markdown(self.content)
         super().save(*args, **kwargs)
+
+    @cached_property
+    def tags(self):
+        return ','.join(self.tag.values_list('name', flat=True))
