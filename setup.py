d25219d82834deef278a879bf7e18ab5d8fb3644
WayneChen1994 <waynechen1994@163.com>
2019-04-03 21:33:41 +0800

    使用Fabric实现项目的自动化打包及部署

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='WayneBlog',
-    version='0.1',
+    version='${version}',
     description='Blog System base on Django',
     author='Wayne.Chen',
     author_email='waynechen1994@163.com',
@@ -16,6 +16,22 @@ setup(
     # include_package_data=True,      # 方法二：配合MANIFEST.in文件
     install_requires=[
         'django~=1.11',
+        'gunicorn==19.8.1',
+        'supervisor==4.0.0dev0',
+        'xadmin==0.6.1',
+        'mysqlclient==1.3.12',
+        'django-ckeditor==5.4.0',
+        'django-rest-framework==0.1.0',
+        'django-redis==4.8.0',
+        'django-autocomplete-light==3.2.10',
+        'mistune==0.8.3',
+        'Pillow==4.3.0',
+        'coreapi==2.3.3',
+        'django-redis==4.8.0',
+        'hiredis==0.2.0',
+        # debug
+        'django-debug-toolbar==1.9.1',
+        'django-silk==2.0.0',
     ],
     extras_require={
         'ipython': ['ipython==6.2.1']
